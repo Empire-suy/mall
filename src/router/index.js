0aac3db89a5273837af1c8a0be9e14d941915795
empire <suy1359@163.com>
2021-02-19 17:27:07 +0800

    初始化并配置 mall项目

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
